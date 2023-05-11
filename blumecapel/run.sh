4e8a2ad7f93f02d7e8046d27b6ceedf2cf0aeb0b
bsfn1844815 <86679938+bsfn1844815@users.noreply.github.com>
2023-05-11 16:35:27 +0200

    Update run.sh

--- blumecapel/run.sh
+++ blumecapel/run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+mkdir outputs
 gcc -g -o blmcp bc_graph.c -lm -lgslcblas -lgsl
 for ((c = 0;c<=40;c=c+1)); 
 do
     T=$(bc<<<"0.75+$c*0.05")
     ./blmcp 8 1 0.25 $T 0.0325 100000 $c;
 done;
+./inferenza 8 100000 0.75 2.75 0.05 41 0
