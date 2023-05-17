e559a5bd0776374722aa8592452985f77ed46854
bsfn1844815 <86679938+bsfn1844815@users.noreply.github.com>
2023-05-17 13:21:30 +0200

    Update run.sh

--- blumecapel/run.sh
+++ blumecapel/run.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
+rm -r outputs
 mkdir outputs
 gcc -g -o blmcp blumecapel.c -lm -lgslcblas -lgsl
 gcc -g -o bcinf bc_inf.c -lm -lgslcblas -lgsl
-for ((c = 0;c<=40;c=c+1)); 
+
+declare -i nMeas=40
+mu=$(bc<<<"0.25")
+declare -i L=8
+J=$(bc<<<"1.0")
+Tmin=$(bc<<<"0.75")
+dT=$(bc<<<"0.05")
+Tmax=$(bc<<<"$Tmin+$dT*$nMeas")
+p=$(bc<<<"0.0325")
+declare -i MCS=100000
+
+echo L = $L, J = $J, mu = $mu, Tmin = $Tmin, Tmax=$Tmax, p = $p,nMeas = $nMeas, MCS = $MCS
+for ((c = 0;c<$nMeas;c=c+1)); 
 do
-    T=$(bc<<<"0.75+$c*0.05")
-    ./blmcp 8 1 0.25 $T 0.0325 100000 $c;
+    T=$(bc<<<"$Tmin+$c*$dT")
+    ./blmcp $L $J $mu $T $p $MCS $c;
     echo -------------------$c----------------------
 done;
-./bcinf 8 100000 0.75 2.75 0.05 41 0
+./bcinf $L $MCS $Tmin $Tmax $dT $nMeas 
